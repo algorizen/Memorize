d4218d6c4d5b0e44f20d872b5f23945933337505
zen <0x11cb@gmail.com>
2020-06-13 23:56:36 +0800

    feat: init project 🎉

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by zen on 5/31/20.
+//  Copyright © 2020 Evoluteix. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        HStack {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    self.viewModel.choose(card: card)
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 2)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(viewModel: EmojiMemoryGame())
+    }
+}
